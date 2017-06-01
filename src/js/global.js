360fa8cf5bf93964043e1a942140891ec86c135c
Moer <199525807@qq.com>
2017-06-01 21:18:48 +0800

    新需求: 购物车\轮播图

---
+++ src/js/global.js
@@ -0,0 +1,10 @@
+$(document).on('click', '.number-pag > .button', function () {
+    let $btn = $(this)
+    let $input = $btn.parent().children('input')
+    let num = Number($input.val())
+    if ($btn.hasClass('add')) {
+        $input.val(num + 1)
+    } else if (num >= 1) {
+        $input.val(num - 1)
+    }
+})
